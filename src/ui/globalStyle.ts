bf798464cb34dd8b3144c9bcbeb40a4e0a12e57d
Tadhg Lewis <tadhg.lewis@outlook.com>
2022-01-19 00:27:34 +1100

    Cleanup

---
+++ src/ui/globalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 16px;
+    font-family: Arial;
+    background-color: white;
+    color: #1d1f20;
+    }
+`;
